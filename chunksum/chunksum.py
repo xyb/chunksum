43c566650a86ec51cf05c72265bc5dd6285d2607
Xie Yanbo <xieyanbo@gmail.com>
2022-11-13 22:02:14 +0800

    improve code complexity (#6)
    * improve code complexity
    
    * improve code complexity
    
    * improve code complexity

--- chunksum/chunksum.py
+++ chunksum/chunksum.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 import os
-import sys
 from hashlib import blake2b
 from hashlib import blake2s
 from hashlib import sha256
 from os.path import getsize
 from os.path import join
 
-from fastcdc.const import AVERAGE_MIN
 from tqdm.auto import tqdm
 from tqdm.utils import CallbackIOWrapper
 
 from .cdc import Chunker
+from .chunksize import GIGA
+from .chunksize import KILO
+from .chunksize import MEGA
 
 
 def iter_file_content(file, size=1024):
@@ -40,74 +41,6 @@ def iter_file_content_progress(file, path, size=1024):
     ) as t:
         fobj = CallbackIOWrapper(t.update, file, "read")
         yield from iter_file_content(fobj, size)
-
-
-class ChunkSizeError(Exception):
-    ...
-
-
-class ChunkSizeTooSmall(ChunkSizeError):
-    ...
-
-
-class ChunkSizeAlign(ChunkSizeError):
-    ...
-
-
-class ChunkSize:
-    """
-    >>> ChunkSize(1024)
-    ChunkSize<1024>
-    >>> ChunkSize(1)  # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
-    Traceback (most recent call last):
-      ...
-    chunksum.chunksum.ChunkSizeError: chunk size too small: 1
-    >>> ChunkSize(1024 + 1)  # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
-    Traceback (most recent call last):
-      ...
-    chunksum.chunksum.ChunkSizeError: chunk size should be a multiple of 4, but 1025 % 5 = 1
-    """  # noqa: E501
-
-    def __init__(self, avg_bytes=AVERAGE_MIN):
-        if (avg_bytes) < AVERAGE_MIN:
-            raise ChunkSizeError(f"chunk size too small: {avg_bytes}")
-        if avg_bytes % 4 != 0:
-            raise ChunkSizeError(
-                "chunk size should be a multiple of 4, "
-                "but {} % 5 = {}".format(
-                    avg_bytes,
-                    avg_bytes % 4,
-                ),
-            )
-        self.avg = avg_bytes
-        self.min = avg_bytes / 4
-        self.max = avg_bytes * 4
-
-    def __repr__(self):
-        """
-        >>> ChunkSize(64 * 1024)
-        ChunkSize<65536>
-        """
-        return f"ChunkSize<{self.avg}>"
-
-    def __mul__(self, x):
-        """
-        >>> ChunkSize(1024) * 64
-        ChunkSize<65536>
-        """
-        return ChunkSize(self.avg * x)
-
-    def __rmul__(self, x):
-        """
-        >>> 2 * ChunkSize(1024)
-        ChunkSize<2048>
-        """
-        return ChunkSize(self.avg * x)
-
-
-KILO = ChunkSize(1024)  # 1KB
-MEGA = KILO * 1024  # 1MB
-GIGA = MEGA * 1024  # 1GB
 
 
 def get_chunker(size_name="", avg=1024, min=256, max=4096):
@@ -185,6 +118,13 @@ def get_hasher(name):
         raise Exception(f"unsupported hash: {name}")
 
 
+def hash_digest_size(data, hasher_name):
+    size = len(data)
+    h = get_hasher(hasher_name)
+    h.update(data)
+    return (h.digest(), size)
+
+
 def compute_file(file, alg_name="fck4sha2", avg=0, min=0, max=0, hash="sha2"):
     """
 
@@ -217,19 +157,13 @@ def compute_file(file, alg_name="fck4sha2", avg=0, min=0, max=0, hash="sha2"):
     else:
         iter_ = iter_file_content(file, size=buffer_size)
 
-    def gen_item(data, hasher_name):
-        size = len(data)
-        h = get_hasher(hasher_name)
-        h.update(data)
-        return (h.digest(), size)
-
     hasher_name = alg_name[len("fck0") :] or hash
     for data in iter_:
         chunker.update(data)
         for chunk in chunker.chunks:
-            result.append(gen_item(chunk, hasher_name))
+            result.append(hash_digest_size(chunk, hasher_name))
     if chunker.tail:
-        result.append(gen_item(chunker.tail, hasher_name))
+        result.append(hash_digest_size(chunker.tail, hasher_name))
     return result
 
 
@@ -239,9 +173,8 @@ def list_hash(digests, hasher_name="sha2"):
     b'\xbe\xf5...\xd9<G!'
     """
     plain = b"".join(digests)
-    h = get_hasher(hasher_name)
-    h.update(plain)
-    return h.digest()
+    digest, _ = hash_digest_size(plain, hasher_name)
+    return digest
 
 
 def format_a_result(path, result, alg_name):
@@ -270,73 +203,3 @@ def walk(target, output_file, alg_name="fck4sha2"):
                 flush=True,
             )
         dirs.sort()
-
-
-def help():
-    print(
-        """Print FastCDC rolling hash chunks and checksums.
-
-Usage: {cmd} <dir> [<alg_name>] > chunksums
-
-alg_name:
-  Format "fc[k|m|g][0-9][sha2|blake2b|blake2s][32]".
-
-  For example, "fck4sha2", means using FastCDC("fc") with an
-  average chunk size of 2**8=256KB("k8") and using sha256("sha2")
-  to calculate the checksum.
-
-  "fcm4blake2b32" means using FastCDC with an average chunk size
-  of 2**4=16MB("m4") and using "blake2b" to calculate and output
-  a checksum of length "32" bytes(save storage).
-
-  For large files, you may using large chunk size, such as "m4",
-  to reduce the number of chunks.
-
-  (default: fck4sha2)
-
-Examples:
-
-  $ {cmd} /etc > ~/etc.chunksums
-
-  $ {cmd} ~/Videos fcm4blake2b32 > ~/Videos/chunksums
-""".format(
-            cmd=sys.argv[0],
-        ),
-    )
-
-
-def main():
-    """
-    >>> sys.argv = ['chunksup']
-    >>> main()  # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
-    Print ...
-    Usage: ...
-    ...
-
-    >>> import tempfile
-    >>> import os.path
-    >>> dir = tempfile.TemporaryDirectory()
-    >>> path = os.path.join(dir.name, 'testfile')
-    >>> _ = open(path, 'wb').write(b'hello')
-    >>> sys.argv = ['chunksum', dir.name]
-    >>> main()
-    9595...3d50  .../testfile  fck4sha2!2cf2...9824:5
-    >>> sys.argv = ['chunksum', dir.name, 'fcm0blake2b32']
-    >>> main()
-    901c...ce59  .../testfile  fcm0blake2b32!324d...72cf:5
-    >>> sys.argv = ['chunksum', dir.name, 'fcm0blake2s']
-    >>> main()
-    8d95...5ee5  .../testfile  fcm0blake2s!1921...ca25:5
-    """
-    if len(sys.argv) == 1:
-        help()
-    else:
-        if len(sys.argv) > 2:
-            path, alg_name = sys.argv[1:3]
-        else:
-            path, alg_name = sys.argv[1], "fck4sha2"
-        walk(path, sys.stdout, alg_name)
-
-
-if __name__ == "__main__":
-    main()  # pragma: no cover
